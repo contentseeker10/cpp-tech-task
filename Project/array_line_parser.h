a6083dba68339a73fbf63f98bac876fb40f0b659
contentseeker10 <appelsene.design@gmail.com>
2026-08-05 18:29:44 +0300

    feat(parser): make functions generic

--- Project/array_line_parser.h
+++ Project/array_line_parser.h
@@ -6,13 +6,40 @@
 * Project
 */
 
-#pragma once
-
-#include <vector>
+#include <stdexcept>
+#include <type_traits>
 #include <string>
 
-using StrVec = std::vector<std::string>;
-using IntVec = std::vector<int>;
+template<typename T>
+T getstrv(const std::string& line, const std::string& delims) {
+	T tokens;
+	size_t begin = line.find_first_not_of(delims);
+	size_t end = line.find_first_of(delims, begin);
 
-StrVec getstrv(const std::string& line, const std::string& delims);
-IntVec getintv(const StrVec& strv);
+	while (begin != std::string::npos) {
+		tokens.push_back(line.substr(begin, end - begin));
+		begin = line.find_first_not_of(delims, end);
+		end = line.find_first_of(delims, begin);
+	}
+
+	return tokens;
+}
+
+template <typename T>
+struct is_string_container : std::false_type {};
+
+template <template <typename...> class C, typename... Args>
+struct is_string_container<C<std::string, Args...>> : std::true_type {};
+
+template <typename T, typename C, typename = typename std::enable_if<is_string_container<C>::value>::type>
+T getintv(const C& strc) {
+	T res;
+	res.reserve(strc.size());
+
+	for (const auto& e : strc) {
+		auto val = std::stoi(e);
+		res.push_back(val);
+	}
+
+	return res;
+}
--- ProjectTest/test.cpp
+++ ProjectTest/test.cpp
@@ -1,14 +1,16 @@
 #include "pch.h"
 
+using StrVec = std::vector<std::string>;
+using IntVec = std::vector<int>;
 TEST(Parser, Fills_StringVector_Successfully) {
 	std::string test_data = "10 20 30\n15 25 35\n100 200 300";
-	StrVec v{ getstrv(test_data, ", ")};
+	StrVec v{ getstrv<StrVec>(test_data, ", ")};
 	EXPECT_FALSE(v.empty());
 }
 
 TEST(Parser, Fills_IntegerVector_Successfully) {
 	std::string test_data = "10 20 30\n15 25 35\n100 200 300";
-	StrVec strv{ getstrv(test_data, ", ") };
-	IntVec intv{ getintv(strv) };
+	StrVec strv{ getstrv<StrVec>(test_data, ", ") };
+	IntVec intv{ getintv<IntVec, StrVec>(strv) };
 	EXPECT_FALSE(intv.empty());
 }
