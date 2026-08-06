cac6e94d3a1b89b5ace61e37c6437f860e4125a7
contentseeker10 <appelsene.design@gmail.com>
2026-08-06 15:46:57 +0300

    feat(transformer): add parameter pack to interface

--- Project/ArrayTransformer.h
+++ Project/ArrayTransformer.h
@@ -12,49 +12,49 @@
 
 #include "csorter.h"
 
-template <typename T>
+template <typename T, typename... Args>
 class ArrayTransformer {
 public:
 	virtual ~ArrayTransformer() = default;
-	virtual void op(T& arr) = 0;
+	virtual void op(T& arr, Args... args) = 0;
 };
 
-template <typename T>
+template <typename T, typename... Args>
 class ArraySorter : public ArrayTransformer<T> {
 public:
-	void op(T& arr) override {
+	void op(T& arr, Args... args) override {
 		custom::sort(arr.begin(), arr.end());
 	}
 };
 
-template <typename T>
+template <typename T, typename... Args>
 class ArrayReverseSorter : public ArrayTransformer<T> {
 public:
-	void op(T& arr) override {
+	void op(T& arr, Args... args) override {
 		custom::sort(arr.rbegin(), arr.rend());
 	}
 };
 
-template <typename T>
+template <typename T, typename... Args>
 class ArrayIntersector : public ArrayTransformer<T> {
 public:
-	void op(T& arr) override {
+	void op(T& arr, Args... args) override {
 
 	}
 };
 
-template <typename T>
+template <typename T, typename... Args>
 class ArrayUnifier : public ArrayTransformer<T> {
 public:
-	void op(T& arr) override {
+	void op(T& arr, Args... args) override {
 
 	}
 };
 
-template <typename T>
+template <typename T, typename... Args>
 class ArrayFilter : public ArrayTransformer<T> {
 public:
-	void op(T& arr) override {
+	void op(T& arr, Args... args) override {
 
 	}
 };
